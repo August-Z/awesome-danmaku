09c064880010e5194053fc415a58fc32a60b0571
August-Z <402279076@qq.com>
2018-10-26 18:39:57 +0800

    todo change options param name

--- flow/control.js
+++ flow/control.js
@@ -10,3 +10,19 @@ declare type DanmakuPlayerOptions = {
   list: Array<string | Object | DnodeOptions>;
   on: { [key: string]: Function };
 }
+
+/**
+ * todo params rename of Player options
+ * last time: 2018.11.11
+ */
+declare type _DanmakuPlayerOptions = {
+  el: ?HTMLElement | string;
+  totalTime: number | string;
+  tag: string;
+  class: string;
+  maxCount: number;
+  valueKey: number | string;
+  rowCount: number;
+  list: Array<string | Object | DnodeOptions>;
+  on: { [key: string]: Function };
+}
